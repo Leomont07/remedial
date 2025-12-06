5800e0d821472476c47b19eed96178cec31bbfb6
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-12-06 02:51:57 -0600

    fix: Correcion 2

--- test/integration.test.cjs
+++ test/integration.test.cjs
@@ -21,7 +21,51 @@ describe('Integración de Endpoints Principales', function() {
             });
     });
 
+    it('POST /api/service/add debe devolver estado 201 y el objeto creado', (done) => {
+        const newService = { 
+            nombre: 'Servicio de Prueba', 
+            descripcion: 'Test de integracion', 
+            precio: 100, 
+            tipo: 'hospedaje',
+            estatus: 1,
+            idUsuario: "886d6971-1b0b-42b1-b9a6-c2e0ee6d74a2"
+        };
+        request(app)
+            .post('/api/service/add')
+            .send(newService)
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.have.property('servicio');
+                expect(res.body.servicio).to.have.property('nombre', 'Servicio de Prueba');
+                
+                done();
+            });
+    });
 
+    it('POST /api/booking/add debería devolver estado 201 y un objeto', (done) => {
+        const validServiceId = "6fbf3af6-6cca-48ea-9d74-8c5f44ad9ce9";
+        const validUserId = "317e3e66-f7a5-40b9-95ef-4a59f630b6fa";
+
+        const newBooking = { 
+            idUsuario: validUserId, 
+            idService: validServiceId, 
+            estatus: 'Pendiente', 
+            cantidad: 1,
+            fechaInicio: "2025-12-10",
+            numNoches: 3,
+            fechaFin: "2025-12-13"
+        };
+        request(app)
+            .post('/api/booking/add') 
+            .send(newBooking)
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.have.property('booking');
+                done();
+            });
+    });
 
     // Eliminamos la prueba GET / y POST /products que causaban 404.
 });
