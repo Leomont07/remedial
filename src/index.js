67f10e20d98c8354c3dd1889fa5334c1e687752b
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-12-06 03:06:28 -0600

    fix: Correcion 4

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import cors from 'cors'
-import { createClient } from '@supabase/supabase-js'
-import authRoutes from './routes/authRoutes.js'
-import serviceRoutes from'./routes/serviceRoutes.js'
-import bookingRoutes from './routes/bookingRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+//import { createClient } from '@supabase/supabase-js'
+//import authRoutes from './routes/authRoutes.js'
+//import serviceRoutes from'./routes/serviceRoutes.js'
+//import bookingRoutes from './routes/bookingRoutes.js'
+//import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 
-export let supabase; 
+// Conexión Supabase
+//export const supabase = createClient(
+//  process.env.SUPABASE_URL,
+//  process.env.SUPABASE_KEY
+//)
 
-try {
-    supabase = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
-    )
-    console.log("INFO: Cliente Supabase inicializado correctamente.");
+// Rutas
+//app.use('/api/auth', authRoutes)
+//app.use('/api/users', userRoutes)
+//app.use('/api/service', serviceRoutes)
+//app.use('/api/booking', bookingRoutes)
 
-} catch (e) {
-    console.error("CRITICAL ERROR: Fallo al inicializar el cliente Supabase.");
-    console.error(e.message);
-    supabase = null; 
-}
-
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/service', serviceRoutes)
-app.use('/api/booking', bookingRoutes)
-
-// RUTA DE SALUD
 app.get('/', (req, res) => { 
   res.status(200).json({ message: 'Bienvenido al server' });
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000 // Usa la variable de entorno o 3000 por defecto
 
 export default app;
+
+//app.listen(PORT, () => {
+  //  console.log(`🚀 Servidor en ejecución en el puerto ${PORT}`);
+//});
