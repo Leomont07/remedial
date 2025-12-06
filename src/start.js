cd8b661ad404d1ec266a94b1ddec5ea53e962e25
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-12-06 03:22:50 -0600

    fix: Correcion 8

--- src/start.js
+++ src/start.js
@@ -1,7 +1,19 @@
 import app from './index.js';
 
+// --- AÑADIR MANEJADORES DE PROCESOS ---
+process.on('uncaughtException', err => {
+  console.error('[CRITICAL] Excepción NO CAPTURADA:', err);
+  // No debes mantener el proceso vivo después de uncaughtException.
+  process.exit(1); 
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('[CRITICAL] Promesa NO MANEJADA:', reason);
+});
+// ------------------------------------
+
 const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0';
+const HOST = '0.0.0.0'; 
 
 app.listen(PORT, HOST, () => {
   console.log(`🚀 Servidor en ejecución en http://${HOST}:${PORT}`);
