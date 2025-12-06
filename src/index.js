e967100957b07c60aebe0c5860476165f2f57675
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-12-06 02:56:23 -0600

    fix: Correcion 3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { createClient } from '@supabase/supabase-js'
-import authRoutes from './routes/authRoutes.js'
-import serviceRoutes from'./routes/serviceRoutes.js'
-import bookingRoutes from './routes/bookingRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+//import { createClient } from '@supabase/supabase-js'
+//import authRoutes from './routes/authRoutes.js'
+//import serviceRoutes from'./routes/serviceRoutes.js'
+//import bookingRoutes from './routes/bookingRoutes.js'
+//import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 
+// Conexión Supabase
+//export const supabase = createClient(
+//  process.env.SUPABASE_URL,
+//  process.env.SUPABASE_KEY
+//)
 
+// Rutas
+//app.use('/api/auth', authRoutes)
+//app.use('/api/users', userRoutes)
+//app.use('/api/service', serviceRoutes)
+//app.use('/api/booking', bookingRoutes)
 
 app.get('/', (req, res) => { 
   res.status(200).json({ message: 'Bienvenido al server' });
